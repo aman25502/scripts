07c4d65c76d1ff22d9f2825e822c8f35bf6740a8
aman25502 <65896145+aman25502@users.noreply.github.com>
2020-07-07 22:49:50 +0530

    Add sftp script

---
+++ sftp.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2020 Dhruv Gera
+import glob
+import sys
+import subprocess
+import select
+import os
+import pysftp
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
+# Your particulars for accessing the sftp client
+myHostname = "frs.sourceforge.net"
+myUsername = os.environ["USERNAME"]
+myPassword = os.environ["PASSWORD"]
+
+# Sourceforge or any other sftp client uploader script
+
+with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, cnopts=cnopts) as sftp:
+    print("Connection successful")
+
+    # Switch to a remote directory
+    sftp.cwd('/home/frs/project/ahoy-builds/gsi')  # Add your dir here
+    # Obtain structure of the remote directory
+    directory_structure = sftp.listdir_attr()
+
+    source = '$ZIP_NAME-GSI-Aonly.7z'  # The path where your file is stored on your drive
+    # The path where you want to upload
+    destination = '/home/frs/project/ahoy-builds/gsi/$ZIP_NAME-GSI-Aonly.7z'
+    sftp.put(source, destination)
+
+    source = '$ZIP_NAME-GSI-AB.7z'  # The path where your file is stored on your drive
+    # The path where you want to upload
+    destination = '/home/frs/project/ahoy-builds/gsi/$ZIP_NAME-GSI-AB.7z'
+    sftp.put(source, destination)
