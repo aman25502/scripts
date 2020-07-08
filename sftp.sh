bf564e78dce8b7f7a533ce7cdf536ec48d6f01c4
aman25502 <65896145+aman25502@users.noreply.github.com>
2020-07-08 14:58:45 +0530

    Sftp script in bash

---
+++ sftp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "exit" | sshpass -p "$sf_pwd" ssh -tto StrictHostKeyChecking=no $sf_usr@shell.sourceforge.net create
+rsync -v --rsh="sshpass -p $sf_pwd ssh -l $sf_usr" output/$ZIP_NAME-GSI-Aonly.7z $sf_usr@shell.sourceforge.net:/home/frs/project/ahoy-builds/gsi
